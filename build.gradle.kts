fd971d2136280f516468e1410a4c4aba98c645c8
Jim Kriz <jim@kriz.net>
2024-11-26 22:13:15 -0500

    Updates from 2023 that didn't make it to the template

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
 import org.gradle.api.tasks.testing.logging.TestLogEvent
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 plugins {
     kotlin("jvm") version "2.0.21"
@@ -10,17 +13,90 @@ plugins {
 
 repositories {
     mavenCentral()
+    // 2023 Day 24. I cheated here...
+    maven {
+        url = uri("https://artifacts.itemis.cloud/repository/maven-mps/")
+    }
+    maven {
+        url = uri("https://projects.itemis.de/nexus/content/repositories/OS/")
+    }
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
-    testImplementation("org.jetbrains.kotlinx:kotlinx-serialization-json-jvm:1.7.3")
+    val junitVersion = "5.10.1"
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
+    // 2023 Day 24. I cheated here...
+    testImplementation("com.microsoft.z3:java-jar:4.8.8.1")
+    testRuntimeOnly("com.microsoft.z3:libz3.java.linux:4.8.8")
+
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
+    implementation(kotlin("stdlib-jdk8"))
+
+    fileTree("$buildDir/nativeLibs")
 }
 
 tasks {
-    test {
+    register<Exec>("createDay") {
+        group = "advent of code"
+        description = "Create the current day's directory and files from the template"
+        commandLine("sh", "-c", "${project.rootDir}/scripts/create-day.sh")
+    }
+
+    register<Exec>("createReadme") {
+        group = "advent of code"
+        description = "Create the current day's README from the AoC html page"
+        commandLine("sh", "-c", "${project.rootDir}/scripts/generate-readme.sh")
+    }
+
+    register<Copy>("extractZ3NativeLib") {
+        val zipFile = configurations.testRuntimeClasspath.get().files.first { it.name.contains("libz3\\.java\\.linux-.*\\.zip".toRegex()) }
+        val nativeLibsDir = file("$buildDir/nativeLibs")
+
+        from(zipTree(zipFile))
+        into(nativeLibsDir)
+
+        doLast {
+            println("Extracted Z3 ${zipFile.name} native library to $nativeLibsDir")
+        }
+    }
+
+    register<Test>("testDay") {
+        dependsOn("extractZ3NativeLib")
+
+        val javaLibraryPath = System.getProperty("java.library.path")
+        systemProperty("java.library.path", "$buildDir/nativeLibs${File.separatorChar}$javaLibraryPath")
+
+        group = "advent of code"
+        description = "Run the current day's tests"
         useJUnitPlatform()
-        testLogging.events = mutableSetOf(TestLogEvent.PASSED, TestLogEvent.FAILED, TestLogEvent.STANDARD_OUT, TestLogEvent.STANDARD_ERROR)
+        val day = LocalDate.now().format(DateTimeFormatter.ofPattern("dd"))
+        filter {
+            includeTestsMatching("day$day.*")
+        }
+        testLogging {
+            info.events = mutableSetOf(
+                TestLogEvent.PASSED,
+                TestLogEvent.FAILED,
+                TestLogEvent.STANDARD_OUT,
+                TestLogEvent.STANDARD_ERROR
+            )
+            events = info.events
+        }
+    }
+
+    test {
+        dependsOn("extractZ3NativeLib")
+
+        val javaLibraryPath = System.getProperty("java.library.path")
+        systemProperty("java.library.path", "$buildDir/nativeLibs${File.pathSeparatorChar}$javaLibraryPath")
+
+        useJUnitPlatform()
+        testLogging.events = mutableSetOf(
+            TestLogEvent.PASSED,
+            TestLogEvent.FAILED,
+            TestLogEvent.STANDARD_OUT,
+            TestLogEvent.STANDARD_ERROR
+        )
         filter {
             // Don't run tests on the "template" classes
             excludeTestsMatching("dayNN.*")
@@ -32,7 +108,9 @@ tasks {
     }
 
     withType<KotlinCompile> {
-        kotlinOptions.jvmTarget = "19"
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_19)
+        }
         java {
             sourceCompatibility = JavaVersion.VERSION_19
             targetCompatibility = JavaVersion.VERSION_19
